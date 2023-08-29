673d10f16624f38c49552747c07a80ab194e15b6
keckothedragon <flappystar05@gmail.com>
2023-08-28 20:08:41 -0400

    Test

---
+++ Hello.java
@@ -0,0 +1,6 @@
+// for me to test that git is working since ash was having issues
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
