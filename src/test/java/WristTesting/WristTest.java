737132003ddee886830290cbfc88fd679efb5a2d
HiHi3690 <duimstrajakson@gmail.com>
2023-10-01 16:02:48 -0400

    :skull:

--- src/test/java/WristTesting/WristTest.java
+++ src/test/java/WristTesting/WristTest.java
@@ -1,45 +1,47 @@
-package WristTesting;
+//will be fixed in mega-merge
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// package WristTesting;
 
-import java.beans.Transient;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import frc.robot.subsystems.wrist.Wrist;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+// import java.beans.Transient;
+
+// import frc.robot.subsystems.wrist.Wrist;
+// import org.junit.jupiter.api.AfterEach;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
 
 
-public class WristTest {
-    static final double delta = 1e-2;
-    Wrist m_wrist;
+// public class WristTest {
+//     static final double delta = 1e-2;
+//     Wrist m_wrist;
     
-    @BeforeEach
-    void setup() {
+//     @BeforeEach
+//     void setup() {
 
-    }
+//     }
 
-    void sleepSeconds(int seconds){
-        int i = 0;
-        while(i < seconds * 50){
-            sleep(1);
-            i++;
-        }
-    }
+//     void sleepSeconds(int seconds){
+//         int i = 0;
+//         while(i < seconds * 50){
+//             sleep(1);
+//             i++;
+//         }
+//     }
 
-    void sleep(int ticks) {
-        int i = 0;
-        while (i < ticks) {
-            m_wrist.periodic();
-            m_wrist.simulationPeriodic();
-            i++;
-        }
-    }
+//     void sleep(int ticks) {
+//         int i = 0;
+//         while (i < ticks) {
+//             m_wrist.periodic();
+//             m_wrist.simulationPeriodic();
+//             i++;
+//         }
+//     }
 
-    @Test 
-    void testSpeed() {
+//     @Test 
+//     void testSpeed() {
     
-    }
+//     }
 
     // @Test
     // void testBrake(){
@@ -48,4 +50,4 @@ public class WristTest {
     //     assertEquals(true, m_intake.isBrake());
     // }
 
-}
+// }
